ff91f1848e7a5d2dfd3c1a67d4161ea510e36e37
OliPro007 <OliPro007@gmail.com>
2016-05-04 19:15:49 -0400

    Appropriate changes to run on linux
    Tested on a Kubuntu 16.04 VM, with a standard setup of sdl2, SDL2_image, OpenGL and the glm folder in the project root, cmake can generate and clean the executable and the resources from the build folder. cmake has to be run inside the build folder for now.

--- OpenGL Test/Scene.h
+++ OpenGL Test/Scene.h
@@ -5,11 +5,12 @@
 #ifdef WIN32
 #include <GL/glew.h>
 #elif __APPLE__
-#define GL3_PROTOTYPES 1
+#define GL_GLEXT_PROTOTYPES 1
 #include <OpenGL/gl3.h>
 #else
-#define GL3_PROTOTYPES 1
-#include <GL3/gl3.h>
+#define GL_GLEXT_PROTOTYPES 1
+#include <GL/gl.h>
+#include <GL/glext.h>
 #endif
 
 #include <iostream>
